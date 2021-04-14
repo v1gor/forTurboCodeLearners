7dcf8c7be69279abe6ff6d94275deda1c210cfc2
v1gor <arkhat97@gmail.com>
2021-04-14 08:28:53 +0600

    test

---
+++ main.py
@@ -0,0 +1,23 @@
+# a = 'String Madina'
+# b = 771 #integer
+# c = 523.12 #float
+# list1 = [1, 2, 'ASdsd']
+# dictinary = {
+# 	'a' : 'asd',
+# 	'b' : 123
+# }
+
+# boolean = True #False
+# Вывести сумму чтобы ответ был в виде float
+a = '1'
+b = 2.212
+c = 3
+# b = str(b) # a = 1
+# c = a + b
+d = float(a) + b + c
+# print(d) #6.212
+
+# print(c)
+# print(type(c))
+
+# kk = int(kk)
